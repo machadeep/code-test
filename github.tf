9e606d275315aa0f499d30ff8ccbe9cb9684e0d5
pradeep <pradeepgoud122@gmail.com>
2024-05-05 21:41:31 +0530

    updated code

--- github.tf
+++ github.tf
@@ -7,18 +7,26 @@ terraform {
   }
 }
 
+
+# provider "github" {
+#   version = "~> 5.0"
+# }
+
 # Configure the GitHub Provider
 
 
 #provider "github" {
-    #token = "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"
-    #my_secret = var.my_secret
+#token = "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"
+#my_secret = var.my_secret
 #}
 
-resource "example_resource" "example" {
-  my_secret = var.my_secret
-}
 
+
+variable "my_secret" {}
+
+variable "terraform_aws_lambda_dead_letter_target_arn" {
+  default = ""
+}
 #variable "my_secret" {}
 
 resource "github_repository" "example" {
